cb567d5d3d75d5c61ed954a399b80cc21b5cf928
Daniel Kidger <daniel.kidger@gmail.com>
2023-11-28 16:16:37 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ echo "Checkout the latest copy of StorwageScaleVagrant"
 git clone https://github.com/IBM/StorageScaleVagrant.git
 cd StorageScaleVagrant
 
-echo "copying it a download of Storage Scale Dedveloper edition"
+echo "copying a download of Storage Scale Dedveloper edition"
 #
 # Really we should be getting thius direct from IBM ?
 #
-(cd software && unzip ~/Downloads/Storage_Scale_Developer-5.1.8.2-x86_64-Linux.zip)
+(cd software && unzip -o ~/Downloads/Storage_Scale_Developer-5.1.8.2-x86_64-Linux.zip)
 
 patch  -p1 -i ../vagrantfile8.patch
