c9f3f530aa652dbf3e7a18623ef201aaabddd2ec
Daniel Kidger <daniel.kidger@gmail.com>
2023-11-28 17:49:27 +0000

    Update install_scale.sh

--- install_scale.sh
+++ install_scale.sh
@@ -7,7 +7,7 @@ if test -d StorageScaleVagrant; then
    echo "change directory"
    cd StorageScaleVagrant
 fi
-cd StorageScaleVagrant
+#cd StorageScaleVagrant
 echo "*** copying a download of Storage Scale Dedveloper edition"
 #
 # Really we should be getting this zipfile direct from IBM ?
@@ -24,7 +24,7 @@ cd virtualbox
 
 # clean up step 
 # but be careful if the vmdk files are still there as they might muild up in VirtualBox ?
-vagrant destroy
+vagrant destroy -f
 if test -d disk; then
     rm -rf disk
 fi
