bc85336b4190772d6da408f6c04d8eded8ca1612
Daniel Kidger <daniel.kidger@gmail.com>
2023-11-28 17:12:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,22 +4,21 @@
 # but for now lats just script it in bash
 #
 mkdir dan && cd dan
-echo "Checkout the latest copy of StorwageScaleVagrant"
+echo "*** Checkout the latest copy of StorwageScaleVagrant"
 #  use https or ssh ?
 git clone https://github.com/IBM/StorageScaleVagrant.git
 cd StorageScaleVagrant
 
-echo "copying a download of Storage Scale Dedveloper edition"
-#
-# Really we should be getting thius direct from IBM ?
-#
-(cd software && unzip -o ~/Downloads/Storage_Scale_Developer-5.1.8.2-x86_64-Linux.zip)
 
 patch  -p1 -i ../vagrantfile8.patch
 
+echo "*** Fire up Vagrant to cearte the base o/s image"
 cd virtualbox/prep-box
 time vagrant up > install_prepbox.log
+
 ls -lrt
+echo "*** Package up this VM into a Vagrent 'box'"
 time vagrant package StorageScale_base --output StorageScale_base.box
 # optional step
 # vagrant destroy
+echo "*** All Done"
