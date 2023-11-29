1d1ab892f174887d75ea9f73b31d3432683cb50f
Daniel Kidger <daniel.kidger@gmail.com>
2023-11-29 12:29:06 +0000

    Update install_basebox.sh

--- install_basebox.sh
+++ install_basebox.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
-
-# Recommended to run this in 'git bash' on Windows
-# If run under powershell, I think there is a conflict between windows linux subsystem and opensssl ?
-
-if test -d StorageScaleVagrant; then
-   echo "change directory"
-   cd StorageScaleVagrant
-fi
-#cd StorageScaleVagrant
-echo "*** copying a download of Storage Scale Dedveloper edition"
 #
-# Really we should be getting this zipfile direct from IBM ?
+# I really would have prefered to do this using Ansible playbooks
+# but for now lats just script it in bash
 #
-BASE="Storage_Scale_Developer-5.1.8.2-x86_64-Linux"
-ls -lrt software
-if [ ! -f software/${BASE}-install ]; then
-   (cd software && unzip -o ~/Downloads/$BASE.zip)
-fi
+#mkdir dan && cd dan
+echo "*** Checkout the latest copy of StorwageScaleVagrant"
+#  use https or ssh ?
+git clone https://github.com/IBM/StorageScaleVagrant.git
+cd StorageScaleVagrant
 
-echo "*** Hack : stop the regeneration of SSL Vagrant keys. They end up in the wrong file format"
-patch  -p1 -i ../vagrant.patch
 
-cd virtualbox
+patch  -p1 -i ../vagrantfile8.patch
 
-# clean up step 
-# but be careful if the vmdk files are still there as they might muild up in VirtualBox ?
-vagrant destroy -f
-if test -d disk; then
-    rm -rf disk
-fi
-pwd
-time vagrant up
+echo "*** Fire up Vagrant to create the base o/s image"
+cd virtualbox/prep-box
+time vagrant up > install_prepbox.log
+
+ls -lrt
+echo "*** Package up this VM into a Vagrent 'box'"
+time vagrant package StorageScale_base --output StorageScale_base.box
+# optional step
+# vagrant destroy
+echo "*** All Done"
+
