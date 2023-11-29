cb3cde3b2aea841450802cc4d9246d799d04fe65
Daniel Kidger <daniel.kidger@gmail.com>
2023-11-29 11:12:46 +0000

    Update install_scale.sh

--- install_scale.sh
+++ install_scale.sh
@@ -13,6 +13,7 @@ echo "*** copying a download of Storage Scale Dedveloper edition"
 # Really we should be getting this zipfile direct from IBM ?
 #
 BASE="Storage_Scale_Developer-5.1.8.2-x86_64-Linux"
+ls -lrt software
 if [ ! -f software/$(BASE)-Linux-install ]; then
    (cd software && unzip -o ~/Downloads/$BASE.zip)
 fi
