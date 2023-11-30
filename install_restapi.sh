283ea8709bdeaf96afb58a41470adbf0c9be7640
Daniel Kidger <daniel.kidger@gmail.com>
2023-11-30 17:53:30 +0000

    Update install_restapi.sh

--- install_restapi.sh
+++ install_restapi.sh
@@ -9,3 +9,10 @@ git clone git@github.com:dannyk96/SpectrumScaleRestAPI.git
 
 # From mobaxterm (/vib/curl appears broken)
 # /drives/c/Program\ Files/Git/mingw64/bin/curl --insecure -u "admin:admin001" https://127.0.0.1:8888/scalemgmt/v2/filesystems
+
+# From Powershell
+# Invoke-WebRequest -SkipCertificateCheck  -URI  https://127.0.0.1:8888/scalemgmt/v2/filesystems -Headers $Headers
+# after first encoding the username and password via 
+#   $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+# $basicAuthValue = "Basic $encodedCreds"
+# $Headers = @{ Authorization = $basicAuthValue
