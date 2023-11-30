02557db3073a3d41d113c47cc776756a903862a2
Daniel Kidger <daniel.kidger@gmail.com>
2023-11-30 17:44:16 +0000

    Update install_restapi.sh

--- install_restapi.sh
+++ install_restapi.sh
@@ -6,3 +6,6 @@ git clone git@github.com:dannyk96/SpectrumScaleRestAPI.git
 
 # Also from powershell
 # 'C:\Program Files\Git\mingw64\bin/curl' --insecure -u "admin:admin001" https://127.0.0.1:8888/scalemgmt/v2/filesystems
+
+# From mobaxterm (/vib/curl appears broken)
+# /drives/c/Program\ Files/Git/mingw64/bin/curl --insecure -u "admin:admin001" https://127.0.0.1:8888/scalemgmt/v2/filesystems
