2219a4d9ccb161ec39b12e48bb8dc3b7f52957cb
Daniel Kidger <daniel.kidger@gmail.com>
2023-12-06 16:48:33 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,17 +15,18 @@ time ./install_basebox.sh
 #
 # Demos
 # =====
-#  1   Query f/s   : mmlsmount, mmlsfs, curl
-#  2   Addding NSDs: mmlspool,mmdf, add 2 NSDs, mmlspool,mmdf
-#  3   Placement   : mmchpolicy (based on file extension), mmlsattr
-#  4   (There is no script 4)
-#  5   Users : 2*groupadd, 8*adduser, 2*mmcrfileset, 6* mkdir, dd, 6*chown
-#  6   snapshots: mmcrfileset, mmcrfileset, mmlssnapshot, cat filess
-#  7   (There is no script 6)
-#  8   Openstack : openstack user+credential create, project create, role add, but no file access ?
-# 80   GUI admin for ACLs: cli/mksuer, cli/chuser
-# 81   Triggers : 2* mmcrfileset (limit 1024 files(, 850*touch , 950*touch
+#  1   Query f/s     : mmlsmount, mmlsfs, curl
+#  2   Addding NSDs  : mmlspool,mmdf, add 2 NSDs, mmlspool,mmdf
+#  3   Placement     : mmchpolicy (based on file extension), mmlsattr
+#  4                   (There is no script 4)
+#  5   Users         : 2*groupadd, 8*adduser, 2*mmcrfileset, 6* mkdir, dd, 6*chown
+#  6   snapshots:      mmcrfileset, mmcrfileset, mmlssnapshot, cat filess
+#  7                   (There is no script 6)
+#  8   Openstack     : openstack user+credential create, project create, role add, but no file access ?
+# 80   GUI for ACLs  : cli/mksuer, cli/chuser
+# 81   Triggers      : 2* mmcrfileset (limit 1024 files(, 850*touch , 950*touch
 # 99   Quota limits with the cli : mmcheckquota, cli/runtask QUOTA
+
 time ./install_scale.sh
 
 # time ./install_restapi.sh
