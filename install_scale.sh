2f6b864714f785f11b51c0d174f44b4c42a250a8
Daniel Kidger <daniel.kidger@gmail.com>
2023-12-11 15:20:12 +0000

    Update install_scale.sh

--- install_scale.sh
+++ install_scale.sh
@@ -3,6 +3,25 @@
 # Recommended to run this in 'git bash' on Windows
 # If run under powershell, I think there is a conflict between windows linux subsystem and opensssl ?
 
+
+# note the demos that get run for the IBM scripts:
+#
+# Demos
+# =====
+#  1   Query f/s     : mmlsmount, mmlsfs, curl
+#  2   Addding NSDs  : mmlspool,mmdf, add 2 NSDs, mmlspool,mmdf
+#  3   Placement     : mmchpolicy (based on file extension), mmlsattr
+#  4                   (There is no script 4)
+#  5   Users         : 2*groupadd, 8*adduser, 2*mmcrfileset, 6* mkdir, dd, 6*chown
+#  6   snapshots:      mmcrfileset, mmcrfileset, mmlssnapshot, cat filess
+#  7                   (There is no script 6)
+#  8   Openstack     : openstack user+credential create, project create, role add, but no file access ?
+# 80   GUI for ACLs  : cli/mksuer, cli/chuser
+# 81   Triggers      : 2* mmcrfileset (limit 1024 files(, 850*touch , 950*touch
+# 99   Quota limits with the cli : mmcheckquota, cli/runtask QUOTA
+
+
+
 if test -d StorageScaleVagrant; then
    echo "change directory"
    cd StorageScaleVagrant
