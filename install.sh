b21f74e3704795593330561d3b81e2b8d3cb67f4
dannyk96 <dan.kidger@csiltd.co.uk>
2024-12-03 16:53:29 +0000

    cleanup

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+function section () {
+   echo " "
+   echo "==================================================================="
+   echo "                  $@"
+   echo "==================================================================="
+}}
 
 PATH=$PATH:"/c/Program Files/Oracle/VirtualBox/"
 vboxmanage list vms
 vboxmanage list hdds
 exit
 
-# Insta;l; Vagrant
+# Install Vagrant
 # from https://developer.hashicorp.com/vagrant/install?product_intent=vagrant
 # Configure Vagrant
 # this is for virtualbox
@@ -18,20 +24,33 @@ echo vagrant plugin install vagrant-vbguest
 # caveat : some employers block this URL :-(
 # from https://www.virtualbox.org/wiki/Downloads
 
-echo "*** Install the Base VM image, including Scale''s prerequisites"
+
+section "Install the Base VM image, including Scale''s prerequisites"
 time ./install_basebox.sh
 
-echo "*** Install  a VM with Spectrum Scale"
+
+section "Install  a VM with Spectrum Scale"
 time ./install_scale.sh
 
-echo "*** Intsall a client (using the same basebox VM image"
+
+section "Install a client (using the same basebox VM image"
 time ./install_client.sh
+
+
+section "Setup a multicluster mount betweeen the two clusters"
+./setup_multicluster.sh
+
+
+section "Test the Rest API"
+time ./test_restapi.sh
+
 
 # is there anything to intsall - or is it a set of demo restapi calls?
 #
 # Think too about using the RestAPI to set multicluster - that would be very cool
 #
-echo " time ./install_restapi.sh"
+
+echo " (Do not use) time ./install_restapi.sh"
 
 # future
 # some clever restapi stuff etc
