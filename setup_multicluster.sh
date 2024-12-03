968925699994cfb0d6d8cc6f97c87d8de6193664
dannyk96 <dan.kidger@csiltd.co.uk>
2024-12-03 14:59:39 +0000

    cleaned up the printing

--- setup_multicluster.sh
+++ setup_multicluster.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 
-# Set up shortcuts to ssh's (as user vagrant
 #
+# Pretty primt section titles
 #
 function section () {
-echo " "
-echo "***"
-echo "*** $@"
-echo "***"
+   echo " "
+   echo "***"
+   echo "*** $@"
+   echo "***"
 }
+# 
+# Set up shortcuts to ssh's (as user vagrant
+#
 function sshm1 () {
     echo "  $@" >&2
-    #ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i StorageScaleVagrant/virtualbox/.vagrant/machines/*/virtualbox/private_key -p 2222 vagrant@127.0.0.1 $@
     ssh -o StrictHostKeyChecking=no  -i StorageScaleVagrant/virtualbox/.vagrant/machines/*/virtualbox/private_key -p 2222 vagrant@127.0.0.1 $@
 }
 function sshc () {
@@ -20,31 +22,43 @@ function sshc () {
     ssh -o StrictHostKeyChecking=no -i client/.vagrant/machines/*/virtualbox/private_key -p 2200 vagrant@127.0.0.1 $@
 }
 
-#sshm1 hostname
-#sshc hostname
 
-#sshm1 sudo mmlscluster
+cat << EOF
+   This Demo Illustartes how to configured a remote mount from one cluster to the other
+   Steps:
+      1. Get the Public key of each cluster and give to the other
+      2. Tell the storage cluster to accept conenctions from the remote
+      3. Tell the remote cluster the IP address to contact on the storage cluster
+      4. export the filesystem
+      5. set the mount point on teh remote and mount the filesystem
+   See documenation at:  https://www.ibm.com/docs/en/storage-scale/5.1.9?topic=system-mounting-remote-gpfs-file
+EOF
 
-section "Get the public keys of each cluster"
+
+section "1. Get the public keys of each cluster"
 sshm1 sudo cat /var/mmfs/ssl/id_rsa.pub >id_rsa_demo.pub
 sshc  sudo cat /var/mmfs/ssl/id_rsa.pub >id_rsa_wilma_site.pub
 
-section "Give each cluster the public key of the other"
+
+section "   Give each cluster the public key of the other"
 cat id_rsa_wilma_site.pub | sshm1 'cat > /tmp/id_rsa_wilma_site.pub'
 cat id_rsa_demo.pub       | sshc  'cat > /tmp/id_rsa_demo.pub'
 
-section "Tell the storage cluster to allow incoming conenctions from wilma_site"
-#sshm1 ls -ld /tmp/id_rsa_wilma_site.pub
+
+section "2. Tell the storage cluster to allow incoming conenctions from wilma_site"
 sshm1 sudo mmauth add wilma_site.example.com -k /tmp/id_rsa_wilma_site.pub
 
-section "Tell the remote cluster where to mount the filesystem from"
+
+section "3. Tell the remote cluster where to mount the filesystem from"
 #sshc  ls -ld /tmp/id_rsa_demo.pub
 sshc sudo mmremotecluster add demo.example.com -n 10.1.2.11 -k /tmp/id_rsa_demo.pub
 
-section "Export the filesystem from the storage cluster"o
+
+section "4. Export the filesystem from the storage cluster"o
 sshm1 sudo mmauth grant wilma_site.example.com -f fs1 -a rw
 
-section "Mount the filesystem on the remote cluster"
+
+section "5. Mount the filesystem on the remote cluster"
 sshc sudo mmremotefs add fs1 -f fs1 -C demo.example.com -T /ibm/fs1
 sshc sudo mmmount fs1
 sshc df -t gpfs
