df2b937a3a09015fa5367e28c35302fdf3bd3398
dannyk96 <daniel.kidger@gmail.com>
2024-12-09 13:18:05 +0000

    chmod to executable

--- install_client.sh
+++ install_client.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-cd client
 
-cp ../Storage_Scale_Developer-*-Linux.zip setup
+cp Storage_Scale_Developer-*-Linux.zip client/setup
 
-vagrant destroy -f 
-vagrant up  >> install_client.log
+# in a subshell because we have a 'cd' 
+{
+  cd client
+  vagrant destroy -f 
+  vagrant up  >> install_client.log
+}
 
-cat <<EOF
-try:
-alias sshc="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i client/.vagrant/machines/*/virtualbox/private_key -p 2200 vagrant@127.0.0.1"
-EOF
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i client/.vagrant/machines/*/virtualbox/private_key -p 2200 vagrant@127.0.0.1 mmlscluster
+SSHC="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i client/.vagrant/machines/*/virtualbox/private_key vagrant@10.1.2.21"
+pwd
+echo -e "we can ssh in with\n $SSHC"
+
+$SSHC uptime
+$SSHC mmlscluster
+
