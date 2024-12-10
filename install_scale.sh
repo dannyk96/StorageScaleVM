ac7da1d69dc29fb3ef2fbbf55920d2f925b5db0d
dannyk96 <dan.kidger@csiltd.co.uk>
2024-12-10 11:04:06 +0000

    changed from sed to ex  for adding mkuser

--- install_scale.sh
+++ install_scale.sh
@@ -3,6 +3,17 @@
 # Recommended to run this in 'git bash' on Windows
 # If run under powershell, I think there is a conflict between windows linux subsystem and opensssl ?
 
+#
+# Pretty print section titles
+#
+function section () {
+   echo " "
+   echo "==================================================================================================="
+   echo "`date +\%H:%M:%S`  $@"
+   echo "==================================================================================================="
+}
+
+section "1 Download the Vagrant sctipts from  https://github.com/IBM/StorageScaleVagrant.git"
 # Have the Vagrant scripts been downlaoded yet?
 if [ ! -d StorageScaleVagrant ]; then
   git clone https://github.com/IBM/StorageScaleVagrant.git
@@ -11,7 +22,7 @@ cd StorageScaleVagrant
 
 
 
-echo "*** copying a download of Storage Scale Dedveloper edition"
+section "2 Ccopy a download of Storage Scale Dedveloper edition"
 #
 # Really we should be getting this zipfile direct from IBM ?
 #
@@ -25,8 +36,10 @@ BASE="Storage_Scale_Developer-${VERSION}-x86_64-Linux"
 
 # should check that this file exists!
 #
-echo "*** unpack the tarball if not yet done"
-#ls -lrt software
+#
+
+section " unpack the tarball if not yet done"
+
 if [ ! -f software/${BASE}-install ]; then
    (cd software && unzip -o ../../$BASE.zip|| exit 1)
 fi
@@ -37,6 +50,8 @@ if [ ! -f Vagrantfile.save ]; then
   cp Vagrantfile Vagrantfile.save
 fi
 
+section "5. Customise this Vagrantfile"
+
 echo "*** add a line to use version \$VERSION=$VERSION of Scale"
 sed -i.bak "/StorageScale_version =/a\$StorageScale_version = \"$VERSION\"" ../shared/Vagrantfile.common
 
@@ -45,13 +60,21 @@ echo "*** change port 8888 to 4438 to avoid clash with Jupyter Notebooks"
 sed -i.bak 's/host: 8888/host: 4438/' Vagrantfile
 # or instead allow use the `auto_correct: true` option of config.vm.network ?
 
+
 echo "*** disable running of the demos after installing Scale"
 # We will do this later one by one
 sed -i.bak 's|/vagrant/demo/script.sh|#/vagrant/demo/script.sh|' Vagrantfile
 
+
 echo "*** We need to enable the GUI user here (was much later in demo/script-80,sh)"
 # so sed it into the end of install/script-05.sh (after sudo /usr/lpp/mmfs/gui/cli/initgui)
-sed -i.bak '/\/cli\/initgui/asudo /usr/lpp/mmfs/gui/cli/mkuser performance -p monitor -g monitor' StorageScaleVagrant/setup/install/script-05.sh
+#sed -i.bak -e '/initgui/ a\' -e 'sudo /usr/lpp/mmfs/gui/cli/mkuser performance -p monitor -g monitor' StorageScaleVagrant/setup/install/script-05.sh
+printf '%s\n' /initgui/a 'sudo /usr/lpp/mmfs/gui/cli/mkuser performance -p monitor -g monitor' . w q |ex -s StorageScaleVagrant/setup/install/script-05.sh
+
+
+echo "*** redict the output of './spectrumscale install' to a file (as bloats STDOUT here)"
+#sed -i.bak -e '/spectrumscale install/ s/$/ > install.log/' StorageScaleVagrant/setup/install/script-05.sh
+printf '%s\n' '/spectrumscale install/' 's/$/ > install.log' . w q | ex -s StorageScaleVagrant/setup/install/script-05.sh
 
 # Now we need to patch the file to point to this version of Storage Scale
 # StorageScaleVagrant/shared/Vagrantfile.common
@@ -63,7 +86,7 @@ sed -i.bak '/\/cli\/initgui/asudo /usr/lpp/mmfs/gui/cli/mkuser performance -p mo
 
 #echo "*** we would prefer to cut down /vagrant/demo/script.sh to do just a basic install so as fast as possibke?"
 
-echo "*** clean up step: destroy the VM if is already exists"
+section "6 Clean up step: destroy the VM if is already exists"
 # but be careful if the vmdk files are still there as they might build up in VirtualBox ?
 vagrant destroy -f
 #if test -d disk; then
@@ -71,18 +94,24 @@ vagrant destroy -f
 #fi
 pwd
 
-time vagrant up >>install.log
+section "7 Run vagrant (and hence the Spectrum Scale installer"
+# I would prefer to do the dirction just of the ./spectrumscale install
+time vagrant up
 rc=$?
-echo "*** all done"
+#cd ../..
 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i StorageScaleVagrant/virtualbox/.vagrant/machines/*/virtualbox/private_key -p 2222 vagrant@127.0.0.1 mmlscluster
+section "8 Confirm that a Scale cluster has been created"
 
-if [ $rc -eq 0 ]; cat <<EOF 
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i .vagrant/machines/*/virtualbox/private_key -p 2222 vagrant@127.0.0.1 mmlscluster
+
+if [ $rc == 0 ]; then
+cat <<EOF 
      now proceed with:
      - testing the GUI 
      - testing the RestAPI
      - testing teh S3 put/get Interface
      - running the provided demo.sh scripts (extra NSDs, etc.
 EOF
+fi
 
 
