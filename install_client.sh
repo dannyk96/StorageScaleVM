20c9e0227f4f767d545f852f00247ee5ed018c07
dannyk96 <dan.kidger@csiltd.co.uk>
2024-12-10 11:02:09 +0000

    remove >logfile

--- install_client.sh
+++ install_client.sh
@@ -4,12 +4,12 @@
 cp Storage_Scale_Developer-*-Linux.zip client/setup
 
 # in a subshell because we have a 'cd' 
-{
   cd client
   vagrant destroy -f 
-  vagrant up  >> install_client.log
-}
+  vagrant up  
+  cd .. 
 
+pwd
 SSHC="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i client/.vagrant/machines/*/virtualbox/private_key vagrant@10.1.2.21"
 pwd
 echo -e "we can ssh in with\n $SSHC"
