fec150d0d95f5237fad8a8635c876c156be698ee
dannyk96 <dan.kidger@csiltd.co.uk>
2024-12-10 10:11:19 +0000

    subsection titles with datestanps

--- install.sh
+++ install.sh
@@ -1,56 +1,66 @@
 #!/bin/bash
 
+dryrun=.true.
+#
+# Pretty print section titles
+#
 function section () {
    echo " "
-   echo "==================================================================="
-   echo "                  $@"
-   echo "==================================================================="
+   echo "==================================================================================================="
+   echo -e "`date +\%H:%M:%S`  $@"
+   echo "==================================================================================================="
 }
 
+# next if for Windows not MacOS
 PATH=$PATH:"/c/Program Files/Oracle/VirtualBox/"
-vboxmanage list vms
-vboxmanage list hdds
+vboxmanage list vms 
+vboxmanage list hdds| grep -i location 
 
-# Install Vagrant
-# from https://developer.hashicorp.com/vagrant/install?product_intent=vagrant
-# Configure Vagrant
-# this is for virtualbox
+section "2 Install Vagrant from https://developer.hashicorp.com/vagrant/install?product_intent=vagrant"
+
+section "3 Configure Vagrant for virtualbox"
 echo vagrant plugin install vagrant-vbguest
-# this is perhaps more fro clouds with dynamic IP addrresses?
 
-#Install Virtualbox
-# caveat : some employers block this URL :-(
-# from https://www.virtualbox.org/wiki/Downloads
+# this is perhaps more for clouds with dynamic IP addrresses?
 
-
-section "Install the Base VM image, including Scale''s prerequisites"
-#time ./install_basebox.sh
+section "4. Install Virtualbox from https://www.virtualbox.org/wiki/Downloads"
 
 
-section "Install  a VM with Spectrum Scale"
+section "5. Install the Base VM image, including Scale\'s prerequisites"
+if [ ! $dryrun ]; then
+time ./install_basebox.sh
+fi
+
+
+section "6. Install  a VM with Spectrum Scale"
+# note that we should delete the VM if already exists
+if [ ! $dryrun ]; then
 time ./install_scale.sh
+fi
 
-
-section "Install a client (using the same basebox VM image"
+section "7. Install a client (using the same basebox VM image"
+if [ ! $dryrun ]; then
 time ./install_client.sh
+fi
 
 
-section "Setup a multicluster mount betweeen the two clusters"
+section "8. Setup a multicluster mount betweeen the two clusters"
+if [ ! $dryrun ]; then
 ./setup_multicluster.sh
+fi
 
 
-section "Test the Rest API"
+section "9. Test the Rest API"
+if [ ! $dryrun ]; then
 time ./test_restapi.sh
-
+fi
 
 # is there anything to intsall - or is it a set of demo restapi calls?
 #
 # Think too about using the RestAPI to set multicluster - that would be very cool
-#
-
 echo " (Do not use) time ./install_restapi.sh"
 
 # future
 # some clever restapi stuff etc
 
-echo "*** All Done"
+section "*** All Done"
